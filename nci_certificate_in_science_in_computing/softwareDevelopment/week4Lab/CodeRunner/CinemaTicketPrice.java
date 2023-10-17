d26540bd28e05bb9e9423f4b4fe71a175dddbe8a
Diego Braga <diebraga0055@gmail.com>
2023-10-17 11:35:52 -0300

    CinemaTicketPrice

---
+++ nci_certificate_in_science_in_computing/softwareDevelopment/week4Lab/CodeRunner/CinemaTicketPrice.java
@@ -0,0 +1,62 @@
+package nci_certificate_in_science_in_computing.softwareDevelopment.week4Lab.CodeRunner;
+
+import java.util.Scanner;
+
+public class CinemaTicketPrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter the number of tickets needed: ");
+        int numTickets = scanner.nextInt();
+
+        Tickets tickets = new Tickets();
+
+        tickets.setAge(age);
+        tickets.setNumberOfTickets(numTickets);
+
+        tickets.computePrice();
+
+        double totalCost = tickets.getFinalPrice();
+
+        System.out.println("Total cost for " + numTickets + " ticket(s): " + totalCost + " euros");
+
+        scanner.close();
+    }
+}
+
+class Tickets {
+    private int age;
+    private int numberOfTickets;
+    private double finalPrice;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public double getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void computePrice() {
+        if (age < 18) {
+            finalPrice = numberOfTickets * 18.1;
+        } else {
+            finalPrice = numberOfTickets * 18.15;
+        }
+    }
+}
